b411700b4798425a6db8072d8eedbf433eb83300
zyz19 <80141182@qq.com>
2023-07-09 18:10:16 +0800

    在屏幕上给打印数据

--- source/loader/loader_16.c
+++ source/loader/loader_16.c
@@ -1,8 +1,21 @@
-//以下之后会以16位编译
 __asm__(".code16gcc");
 
-void loader_entry(void){
-    for(;;){
+/**
+ * BIOS下显示字符串
+ */
+static void show_msg (const char * msg) {
+    char c;
 
-    }
+	// 使用bios写显存，持续往下写
+	while ((c = *msg++) != '\0') {
+		__asm__ __volatile__(
+				"mov $0xe, %%ah\n\t"
+				"mov %[ch], %%al\n\t"
+				"int $0x10"::[ch]"r"(c));
+	}
+}
+
+void loader_entry(void) {
+    show_msg("wqd qdasdas ....loading.....\n\r");
+    for(;;) {}
 }
