c1764a7d8258834d57beb466299efd8696a7e045
zyz19 <80141182@qq.com>
2023-07-21 20:57:45 +0800

    创建日志打印接口，测试成功

--- source/kernel/include/os_cfg.h
+++ source/kernel/include/os_cfg.h
@@ -7,5 +7,5 @@
 #define KERNEL_STACK_SIZE       (8*1024)    // 内核栈
 
 #define OS_TICK_MS              10       	// 每毫秒的时钟数
-
+#define OS_VERSION              "1.0.0"
 #endif
--- source/kernel/init/init.c
+++ source/kernel/init/init.c
@@ -6,6 +6,8 @@
 #include "cpu/cpu.h"
 #include "cpu/irq.h"
 #include "dev/time.h"
+#include "tools/log.h"
+#include "os_cfg.h"
 
 /**
  * 内核入口
@@ -13,11 +15,14 @@
 void kernel_init (boot_info_t * boot_info) {
     //初始化GDT表
     cpu_init();
+    log_init();
     irq_init();
     time_init();
 }
 
 void init_main(void){
+    log_printf("Kernel is running....");
+    log_printf("Version: %s", OS_VERSION);
     //int i = 3 / 0;
     //irq_enable_global();
     for(;;){}
